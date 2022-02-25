c3754063f6f06a8e779197796675693cc6c169ed
Pauline Vos <pvos88@gmail.com>
2022-02-25 15:00:39 +0000

    Implement basic auth check

---
+++ app.js
@@ -0,0 +1,52 @@
+const apiFixtures = require('./fixtures/api')
+const express = require('express')
+const app = express()
+const port = 3000;
+
+const auth = (req, res, next) => {
+  const key = req.headers['X-API-Key'];
+  const secret = req.headers['X-API-Secret'];
+
+  if (apiFixtures.api.key !== key || apiFixtures.api.secret !== secret) {
+    return res.status(403).send({
+      "error": {
+        "reference": "a26ffe51-e4b8-4a28-8c7b-f4edfd73e12f",
+        "code": 811
+      }
+    });
+  }
+
+  next();
+}
+
+app.post('/payload', (req, res) => {
+  res.send({})
+})
+
+app.post('/payload', (req, res) => {
+  res.send({})
+})
+
+app.post('/payload', (req, res) => {
+  res.send({})
+})
+
+app.get('/ping', auth, (req, res) => {
+  res.send({})
+})
+
+app.get('/curated-assets', (req, res) => {
+  res.send({})
+})
+
+app.get('/kyc-status/:account', (req, res) => {
+  res.send({})
+})
+
+app.get('/xrpl-tx/:txid', (req, res) => {
+  res.send({})
+})
+
+app.listen(port, () => {
+  console.log(`Test server listening on port ${port}`)
+})
